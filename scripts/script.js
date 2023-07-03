570bf8ddb1d63fe303134c54dafc4edf6e03d90d
Anton <53440879+Wespe15@users.noreply.github.com>
2023-07-03 14:46:42 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const title = document.querySelector('.title')
+//     const nav = document.querySelector('.nav')
+//     window.addEventListener('scroll', () => {
+//         let scrollTop = window.scrollY
+//         let titleCenter = title.offsetHeight
+
+//         if (scrollTop >= titleCenter) {
+//             nav.classList.add('fixed');
+//             title.computedStyleMap.marginTop = '${nav.offsetHeight}px'
+//         } else {
+//             nav.classList.remove('fixed');
+//             title.computedStyleMap.marginTop = 'px'
+//         }
+//     });
+// });
